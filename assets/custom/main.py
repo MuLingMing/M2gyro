eaf1c9aa27a6336228f12c47e5535d84a7fd1919
MuLingMing <l7f5m3ng@163.com>
2026-01-11 14:21:30 +0800

    resource

---
+++ assets/custom/main.py
@@ -0,0 +1,28 @@
+"""
+Agent入口
+该文件的作用为：
+将自定义的Agent功能注册到Agent服务器中，便于在调试时使用
+"""
+
+import sys
+from maa.agent.agent_server import AgentServer
+from maa.toolkit import Toolkit
+
+from Agent_file import *
+
+
+def main():
+    Toolkit.init_option("./")
+    if len(sys.argv) > 1:
+        print("使用自定义socket_id: " + sys.argv[-1])
+        socket_id = sys.argv[-1]
+    else:
+        print("使用默认socket_id: MAA_AGENT_SOCKET")
+        socket_id = "MAA_AGENT_SOCKET"
+    AgentServer.start_up(socket_id)
+    AgentServer.join()
+    AgentServer.shut_down()
+
+
+if __name__ == "__main__":
+    main()
